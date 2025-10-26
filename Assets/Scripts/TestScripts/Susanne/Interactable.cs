ee3909b1d58cbd83e1a8c54e9177597add2f041e
Zuzena <98824626+Zuzena@users.noreply.github.com>
2025-10-26 20:16:52 +0200

    updates

--- Assets/Scripts/TestScripts/Susanne/Interactable.cs
+++ Assets/Scripts/TestScripts/Susanne/Interactable.cs
@@ -3,8 +3,19 @@ using UnityEngine.Events;
 
 public class Interactable : MonoBehaviour
 {
-    // !!Later use for effects!!
+    [Header("Click events")]
     public UnityEvent onClicked;
+
+    [Header("Moving objects")]
+    public bool movingObject;
+    public float moveDistance = 2f;
+    public float moveSpeed = 3f;
+
+    private Vector3 startingPosition;
+    private Vector3 targetPosition;
+    private bool isMoving = false;
+    private bool movedLeft = false;
+    private Camera camera;
 
     // Called by GameController when item was interacted with
     public void Clicked()
